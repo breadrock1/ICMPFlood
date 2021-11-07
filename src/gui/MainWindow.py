e45d51175faf3745211f194d1dc30c870a2c4599
Bread White <breadrock1@gmail.com>
2021-11-07 16:20:37 +0300

    Has been renamed vars into gui classes

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -14,48 +14,49 @@ from src.flooder import Flooder
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
+
         self.setWindowTitle('ICMP Packet')
-        self.initGUI()
+        self._buildGUI()
 
-    def initGUI(self):
-        lbl1 = QLabel('Enter IP adress:', self)
-        lbl2 = QLabel('Specify port: ', self)
-        lbl3 = QLabel('Specify packet length: ', self)
-        lbl4 = QLabel('Specify frequency value :', self)
+    def _buildGUI(self):
+        labelAddress = QLabel('Enter IP adress:', self)
+        labelPortNum = QLabel('Specify port: ', self)
+        labelLength = QLabel('Specify packet length: ', self)
+        labelFrequency = QLabel('Specify frequency value :', self)
 
-        self.entry1 = QLineEdit(self)
-        self.entry2 = QLineEdit(self)
-        self.entry3 = QLineEdit(self)
-        self.entry4 = QLineEdit(self)
+        self.editAddress = QLineEdit(self)
+        self.exitPortNum = QLineEdit(self)
+        self.editLength = QLineEdit(self)
+        self.editFrequency = QLineEdit(self)
 
-        btn1 = QPushButton('Send packet', self)
-        btn1.clicked.connect(self.sendTo)
+        buttonSend = QPushButton('Send packet', self)
+        buttonSend.clicked.connect(self.sendTo)
 
-        btn2 = QPushButton('Close', self)
-        btn2.clicked.connect(self.close)
+        buttonClose = QPushButton('Close', self)
+        buttonClose.clicked.connect(self.close)
 
-        grid = QGridLayout()
-        grid.setSpacing(1)
+        gridLayout = QGridLayout()
+        gridLayout.setSpacing(1)
 
-        grid.addWidget(lbl1, 0, 0)
-        grid.addWidget(lbl2, 1, 0)
-        grid.addWidget(lbl3, 2, 0)
-        grid.addWidget(lbl4, 3, 0)
-        grid.addWidget(self.entry1, 0, 1)
-        grid.addWidget(self.entry2, 1, 1)
-        grid.addWidget(self.entry3, 2, 1)
-        grid.addWidget(self.entry4, 3, 1)
-        grid.addWidget(btn1, 5, 0, 1, 3)
-        grid.addWidget(btn2, 6, 2, 1, 1)
+        gridLayout.addWidget(labelAddress, 0, 0)
+        gridLayout.addWidget(labelPortNum, 1, 0)
+        gridLayout.addWidget(labelLength, 2, 0)
+        gridLayout.addWidget(labelFrequency, 3, 0)
+        gridLayout.addWidget(self.editAddress, 0, 1)
+        gridLayout.addWidget(self.exitPortNum, 1, 1)
+        gridLayout.addWidget(self.editLength, 2, 1)
+        gridLayout.addWidget(self.editFrequency, 3, 1)
+        gridLayout.addWidget(buttonSend, 5, 0, 1, 3)
+        gridLayout.addWidget(buttonClose, 6, 2, 1, 1)
 
-        self.setLayout(grid)
+        self.setLayout(gridLayout)
         self.setGeometry(600, 470, 600, 400)
         self.show()
 
     def sendTo(self):
-        ip = str(self.entry1.text())
-        port = int(self.entry2.text())
-        length = int(self.entry3.text())
-        freq = float(self.entry4.text())
+        addr = str(self.editAddress.text())
+        port = int(self.exitPortNum.text())
+        length = int(self.editLength.text())
+        freq = float(self.editFrequency.text())
 
-        Flooder(ip, port, length, freq, 1)
+        Flooder(addr, port, length, freq, 1)
