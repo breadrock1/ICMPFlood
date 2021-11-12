2a934930630500c3be0a49b44550f131db1aede8
Bread White <breadrock1@gmail.com>
2021-11-12 12:55:20 +0300

    Has been fixed trouble with showing MainWindow.

--- src/gui/MainWindow.py
+++ src/gui/MainWindow.py
@@ -1,10 +1,8 @@
 from PyQt5.QtWidgets import (
-    QMainWindow,
     QLabel,
     QGridLayout,
     QPushButton,
     QWidget,
-    QApplication,
     QLineEdit
 )
 
@@ -16,21 +14,24 @@ class MainWindow(QWidget):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle('ICMP Packet')
-        self._buildGUI()
+        self.setLayout(self._buildGUI())
+        self.setGeometry(600, 470, 600, 400)
 
-    def _buildGUI(self):
-        labelAddress = QLabel('Enter IP adress:', self)
-        labelPortNum = QLabel('Specify port: ', self)
-        labelLength = QLabel('Specify packet length: ', self)
-        labelFrequency = QLabel('Specify frequency value :', self)
+    def _buildGUI(self) -> QGridLayout:
+        labelAddress = QLabel('IP-address: ', self)
+        labelPortNum = QLabel('Port number: ', self)
+        labelLength = QLabel('Packet length: ', self)
+        labelFrequency = QLabel('Frequency: ', self)
+        labelThreads = QLabel('Threads: ', self)
 
         self.editAddress = QLineEdit(self)
         self.exitPortNum = QLineEdit(self)
         self.editLength = QLineEdit(self)
         self.editFrequency = QLineEdit(self)
+        self.editThreads = QLineEdit(self)
 
         buttonSend = QPushButton('Send packet', self)
-        buttonSend.clicked.connect(self.sendTo)
+        buttonSend.clicked.connect(self._sendTo)
 
         buttonClose = QPushButton('Close', self)
         buttonClose.clicked.connect(self.close)
@@ -42,21 +43,30 @@ class MainWindow(QWidget):
         gridLayout.addWidget(labelPortNum, 1, 0)
         gridLayout.addWidget(labelLength, 2, 0)
         gridLayout.addWidget(labelFrequency, 3, 0)
+        gridLayout.addWidget(labelThreads, 4, 0)
+
         gridLayout.addWidget(self.editAddress, 0, 1)
         gridLayout.addWidget(self.exitPortNum, 1, 1)
         gridLayout.addWidget(self.editLength, 2, 1)
         gridLayout.addWidget(self.editFrequency, 3, 1)
+        gridLayout.addWidget(self.editThreads, 4, 1)
+
         gridLayout.addWidget(buttonSend, 5, 0, 1, 3)
         gridLayout.addWidget(buttonClose, 6, 2, 1, 1)
 
-        self.setLayout(gridLayout)
-        self.setGeometry(600, 470, 600, 400)
-        self.show()
+        return gridLayout
 
-    def sendTo(self):
-        addr = str(self.editAddress.text())
-        port = int(self.exitPortNum.text())
-        length = int(self.editLength.text())
-        freq = float(self.editFrequency.text())
+    def _sendTo(self):
+        address = str(self.editAddress.text())
+        port_number = int(self.exitPortNum.text())
+        packet_length = int(self.editLength.text())
+        frequency = float(self.editFrequency.text())
+        threads = int(self.editThreads.text())
 
-        Flooder(addr, port, length, freq, 1)
+        flooder = Flooder(threads=threads)
+        flooder.run_flooding(
+            ip=address,
+            port=port_number,
+            length=packet_length,
+            frequency=frequency
+        )
