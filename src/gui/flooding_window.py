1f7033db277171906292840befe61905646beaa6
Bread White <breadrock1@gmail.com>
2021-12-11 02:35:38 +0300

    Has been added docstrings into all classes

--- src/gui/flooding_window.py
+++ src/gui/flooding_window.py
@@ -13,6 +13,32 @@ from src.flooder import Flooder
 
 
 class FloodingWindow(QWidget):
+    """
+    This class extends PyQt5.QtWidgets.QWidget class which provides ability to build
+    and show GUI window. This class build window which contains information about
+    running flooding process.
+
+    :argument:
+        args (Dict[str, Any]):
+            The arguments which user has been entered to flood.
+        parent:
+            The parent object (default = None).
+
+    :attributes:
+        all_threads (List):
+            This container stored all running flooding threads.
+        address (AnyStr):
+            The target ip-address.
+        port (int):
+            The target port number.
+        length (int):
+            The length to build ICMP-packet.
+        frequency (float):
+            The frequency to send ICMP-packets.
+        num_threads (int):
+            The amount of threads to flood.
+    """
+
     def __init__(self, args: Dict[str, Any], parent=None):
         QWidget.__init__(self, parent)
 
@@ -28,6 +54,14 @@ class FloodingWindow(QWidget):
         self.setWindowModality(QtCore.Qt.ApplicationModal)
 
     def _buildGUI(self) -> QGridLayout:
+        """
+        This method creates, configures and returns QGridLayout object with
+        replaced into GUI elements.
+
+        :return:
+            QGridLayout object.
+        """
+
         self.setWindowTitle('Flooding')
 
         labelAddress = QLabel('Sending...', self)
@@ -43,15 +77,36 @@ class FloodingWindow(QWidget):
 
         return gridLayout
 
-    def show_window(self):
+    def show_window(self) -> None:
+        """
+        This method provides ability to show initialized GUI window
+        from another class which is invoked this method.
+        """
+
         self.__sendTo()
         self.show()
 
-    def __close__all_threads(self):
+    def __close__all_threads(self) -> None:
+        """
+        This method just terminates all running threads.
+        """
+
         [self.thread.terminate() for self.thread in self.all_threads]
         self.close()
 
-    def __build_flooder_thread(self):
+    def __sendTo(self) -> None:
+        """
+        There is wrapper method to code simplistic.
+        """
+
+        [self.__build_flooder_thread()\
+            for number in range(0, self.num_threads)]
+
+    def __build_flooder_thread(self) -> None:
+        """
+        This method just run flooding into another thread.
+        """
+
         self.thread = QThread()
         self.flooder = Flooder(
             address=self.address,
@@ -68,7 +123,3 @@ class FloodingWindow(QWidget):
 
         self.all_threads.append(self.thread)
         self.thread.start()
-
-    def __sendTo(self):
-        [self.__build_flooder_thread()\
-            for number in range(0, self.num_threads)]
