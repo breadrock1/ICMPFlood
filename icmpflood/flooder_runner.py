bec2df74c15f9f3805b4087f191331107d69d015
Bread White <breadrock1@gmail.com>
2023-03-12 22:36:21 +0300

    Updated FlooderRunner class after all changes into Flooder.

--- icmpflood/flooder_runner.py
+++ icmpflood/flooder_runner.py
@@ -1,56 +1,71 @@
-from typing import Dict, Any
-from threading import Thread, Event
+from datetime import datetime
+from logging import error, warning
+from typing import Any, Dict, List
 
 from icmpflood.flooder import Flooder
 
 
-class FlooderConsoleRunner(Thread):
+class FlooderRunner:
     """
     This class extends threading.Thread class which provides ability to run
     any class with another thread. This class runs flooding with another threads.
     """
 
-    threads_number: int
-    """The amount of threads to flood."""
-
-    arguments: Dict[str, Any]
-    """The arguments which user has been entered to flood."""
+    JOIN_TIMEOUT = 5
 
     def __init__(self, threads_number: int, arguments: Dict[str, Any]):
-        Thread.__init__(self)
+        """
+        The FlooderRunner class constructor.
 
-        self.args = arguments
+        Args:
+            threads_number (int): The amount of target threads.
+            arguments (Dict[str, Any]): The dict of arguments for Flooder class.
+
+        """
+
+        self.arguments = arguments
         self.threads_num = threads_number
 
-        self.all_threads = list()
-        self.flooder = Flooder(
-            address=self.args.get('ip'),
-            port_number=self.args.get('port'),
-            packet_length=self.args.get('length'),
-            sending_frequency=self.args.get('frequency')
-        )
+        self._threads: List[Flooder] = []
 
-    def run(self):
+    def _interrupt_threads(self):
+        """
+        This method interrupts all running threads.
+        """
+        for thread in self._threads:
+            thread.shutdown_flag.set()
+            thread.join(FlooderRunner.JOIN_TIMEOUT)
+
+        self._threads.clear()
+
+    def _launch_threads(self):
+        """
+        This method initializing multiple threads by passed threads number option.
+        """
+        for thread_iter in range(0, self.threads_num):
+            thread = Flooder(name=f'thread-{thread_iter}', arguments=self.arguments)
+            self._threads.append(thread)
+            thread.start()
+
+    def launch_flooder(self):
         """
         This method runs with another thread to create ICMP-packet and send it
         to specified target ip-address.
         """
 
-        interrupt_event = Event()
+        try:
+            start_time = datetime.now()
+            self._launch_threads()
+            while True:
+                print('Packets sending duration: {}'.format(datetime.now() - start_time), end='\r')
+                pass
 
-        for thread_iter in range(0, self.threads_num):
+        except KeyboardInterrupt as err:
+            warning(msg='\nHas been triggered keyboard interruption!')
+            warning(msg='Terminating all running threads...')
 
-            thread = Thread(
-                daemon=True,
-                target=self.flooder.run,
-                name=f'flooding-cmd-thread-{thread_iter}',
-                args=(
-                    self.args.get('ip'),
-                    self.args.get('port'),
-                    self.args.get('length'),
-                    self.args.get('frequency'),
-                )
-            )
+        except Exception as err:
+            error(msg=f'Has been caught unknown runtime error: {err}')
 
-            thread.start()
-            interrupt_event.wait()
+        finally:
+            self._interrupt_threads()
